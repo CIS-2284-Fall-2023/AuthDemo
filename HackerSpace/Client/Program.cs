7eaf9c20cf81b7211d6004baeb58c7eaf8b7c338
Rob <rgarner011235@gmail.com>
2023-11-06 18:16:05 -0700

    Added public and protected HTTP Clients

--- HackerSpace/Client/Program.cs
+++ HackerSpace/Client/Program.cs
@@ -13,8 +13,9 @@ namespace HackerSpace.Client
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddHttpClient("HackerSpace.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+            builder.Services.AddHttpClient("HackerSpace.ProtectedServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+            builder.Services.AddHttpClient("HackerSpace.PublicServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
             // Supply HttpClient instances that include access tokens when making requests to the server project
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("HackerSpace.ServerAPI"));
