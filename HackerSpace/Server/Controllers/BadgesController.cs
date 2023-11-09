e4f4758997d2970722b68ce41f06c44384dd1653
Rob Garner <rgarner011235@gmail.com>
2023-11-08 19:13:11 -0700

    Added list of badges to page

---
+++ HackerSpace/Server/Controllers/BadgesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HackerSpace.Server.Data;
+using HackerSpace.Shared.Models;
+
+namespace HackerSpace.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BadgesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BadgesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Badges
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Badge>>> GetBadges()
+        {
+          if (_context.Badges == null)
+          {
+              return NotFound();
+          }
+            return await _context.Badges.ToListAsync();
+        }
+
+        // GET: api/Badges/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Badge>> GetBadge(int id)
+        {
+          if (_context.Badges == null)
+          {
+              return NotFound();
+          }
+            var badge = await _context.Badges.FindAsync(id);
+
+            if (badge == null)
+            {
+                return NotFound();
+            }
+
+            return badge;
+        }
+
+        // PUT: api/Badges/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBadge(int id, Badge badge)
+        {
+            if (id != badge.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(badge).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BadgeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Badges
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Badge>> PostBadge(Badge badge)
+        {
+          if (_context.Badges == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Badges'  is null.");
+          }
+            _context.Badges.Add(badge);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBadge", new { id = badge.Id }, badge);
+        }
+
+        // DELETE: api/Badges/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBadge(int id)
+        {
+            if (_context.Badges == null)
+            {
+                return NotFound();
+            }
+            var badge = await _context.Badges.FindAsync(id);
+            if (badge == null)
+            {
+                return NotFound();
+            }
+
+            _context.Badges.Remove(badge);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BadgeExists(int id)
+        {
+            return (_context.Badges?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
