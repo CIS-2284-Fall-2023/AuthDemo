202e095ae7227ed06bc33ceeda68d037fd1c4c0d
Rob <rgarner011235@gmail.com>
2023-11-13 18:17:12 -0700

    Added dbset

--- HackerSpace/Server/Data/ApplicationDbContext.cs
+++ HackerSpace/Server/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Duende.IdentityServer.EntityFramework.Options;
 using Hackerspace.Shared.Models;
 using HackerSpace.Server.Models;
+using HackerSpace.Shared.Models;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
@@ -10,6 +11,7 @@ namespace HackerSpace.Server.Data
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
         public DbSet<Post> Posts { get; set; }
+        public DbSet<Badge> Badges { get; set; }
 
         public ApplicationDbContext(
             DbContextOptions options,
