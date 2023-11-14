38dc971a0cd48568c38c1a2c2c831d259300cffb
Rob <rgarner011235@gmail.com>
2023-11-13 18:30:24 -0700

    modified badge

--- HackerSpace/Shared/Models/Badge.cs
+++ HackerSpace/Shared/Models/Badge.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace HackerSpace.Shared.Models
     public class Badge
     {
         public int Id { get; set; }
+
+        [MinLength(10)]
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string? ImagePath { get; set; }
