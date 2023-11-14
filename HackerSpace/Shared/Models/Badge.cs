ccfe60253d3ce857317c2869f0903c9b45a97688
Rob <rgarner011235@gmail.com>
2023-11-13 18:14:57 -0700

    Added badge model

---
+++ HackerSpace/Shared/Models/Badge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerSpace.Shared.Models
+{
+    public class Badge
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; } 
+    }
+}
